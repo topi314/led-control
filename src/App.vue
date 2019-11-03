1ad962def427a7839dab18e95aa8d5a5554c1299
Tobias Schultheiß <tobi@strasserhof.com>
2019-11-03 01:23:04 +0100

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="app">
+    <led-control />
+  </div>
+</template>
+
+<script>
+import LedControl from './components/LedControl'
+
+export default {
+  name: 'app',
+  components: {
+    LedControl
+  }
+}
+</script>
+
+<style>
+body, html{
+  background-color: #2c3e50;
+  margin: 0;
+}
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: white;
+  height: 100%;
+}
+</style>
