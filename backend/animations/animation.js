7b6065a58e62b9f7650a2e0aaf7806d6c80678cc
Tobias Schultheiß <tobi@strasserhof.com>
2020-01-07 22:01:14 +0100

    added example aniamtion script

---
+++ backend/animations/animation.js
@@ -0,0 +1,23 @@
+/* Import ws2801 library and our config file */
+import pixels from 'rpi-ws2801'
+import config from '../config'
+
+/********************************
+ * Make Connection to LED-Stripe 
+ * Initalze Variables here
+ ********************************/
+pixels.connect(config.leds)
+let r = 0
+let g = 0
+let b = 0
+
+/* Execute run Method each 1000ms */
+setInterval(run, 1000)
+
+/* Run Method (executed each frame) */
+function run() {
+    r = Math.floor(Math.random() * 256)
+    g = Math.floor(Math.random() * 256)
+    b = Math.floor(Math.random() * 256)
+    pixels.fill(r, g, b)
+}
