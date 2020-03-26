140e5afd94bc35ad4c35b70d5d0bcf13403b8130
TopiSenpai <tobi@strasserhof.com>
2020-03-26 23:06:01 +0000

    fixed rainbow_cycle effect

--- backend/src/animations/rainbow_cycle.js
+++ backend/src/animations/rainbow_cycle.js
@@ -5,16 +5,24 @@ module.exports = class RainbowCycle extends Animation{
     constructor(pixels, config) {
         super()
         let val = 360 / config.leds
+        let index = 0
         let hue = 0
         let saturation = 100
         let luminosity = 50
         super.start(() => {
-            hue = val
+            hue = val * index
             for (let i = 0; i < config.leds; i++) {
                 pixels.setColor(i, colorConvert.hsl.rgb([hue, saturation, luminosity]))
                 hue += val
+                if(hue > 360) {
+		    hue = 0
+		}
             }
-            pixels.update()
+            index++
+            if(index > config.leds) {
+		index = 0
+	    }
+	    pixels.update()
         }, 100)
     }
 }
